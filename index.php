5aab279014e661e20375d1303e7c0df2cdc66ab4
ishak <ijmilou@roc-dev.com>
2021-09-22 15:02:14 +0200

    added logo of homepage.

---
+++ index.php
@@ -0,0 +1,28 @@
+<!DOCTYPE html>
+<html lang="en">
+<head>
+    <link href="style.css" rel="stylesheet">
+    <meta charset="UTF-8">
+    <title>Title</title>
+</head>
+<body>
+    <img id="headerImg" src="images/img.png" alt="homeImage marvel">
+
+    <div class="movies">
+        <div class="container">
+            <img src="images/antmanHome.jpg">
+            <p class="text"></p>
+            <img src="images/blackPantherHome.jpg">
+            <img src="images/doctorStrangeHome.jpg">
+            <img src="images/eternalsHome.jpg">
+            <img src="images/hawkeyeHome.webp">
+            <img src="images/spidermanHome.jpg">
+            <img src="images/thorHome.jpg">
+            <img src="images/whatifHome.png">
+        </div>
+    </div>
+</body>
+</html>
+
+<?php
+?>

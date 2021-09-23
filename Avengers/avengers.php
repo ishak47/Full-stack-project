a10816a66effc07b8a6c461684ecdf45f7e594a1
ishak <ijmilou@roc-dev.com>
2021-09-23 14:43:56 +0200

    subpage almost done

--- Avengers/avengers.php
+++ Avengers/avengers.php
@@ -1,17 +1,79 @@
 <!DOCTYPE html>
 <html lang="en">
 <head>
+    <link href="Avengers.css" rel="stylesheet">
     <meta charset="UTF-8">
-    <title>Avengers</title>
-    <link href="image/avengersIcon.png" type="image/icon" rel="icon">
-
+    <title>Marvel</title>
+    <link href="images/img.png" type="image/icon" rel="icon">
 </head>
 <body>
+<header>
+    <img id="headerImg" src="../images/img.png" alt="homeImage marvel">
 
-hello world
+    <nav>
+        <a href="Avengers/avengers.php">Avengers</a>
+        <a href="captainAmerica">captain America</a>
+        <a href="thor">thor</a>
+        <a href="ironMan">iron Man</a>
+        <a href="blackPanther">black Panther</a>
+    </nav>
+</header>
+<div id="HomeMainBody">
+    <div class="titels">
+        <h1>avengers</h1>
+        <h1>age of ultron</h1>
+        <h1>infinity war</h1>
+        <h1>endgame</h1>
+    </div>
+    <div class="container">
+        <div class="column">
+            <img src="../Avengers/image/avengers.jpg">
+            <p>Lorem ipsum dolor sit amet, consectetur adipiscing elit. Donec varius malesuada ante, id vehicula felis.
+                Cras sit amet neque orci. Ut sit amet euismod leo. Orci varius natoque penatibus et magnis dis
+                parturient montes, nascetur ridiculus mus. Mauris mattis neque sed magna scelerisque cursus. Aliquam
+                tincidunt dignissim quam ac lacinia. Nulla quis posuere ex. Aliquam non lacinia enim. Vestibulum posuere
+                vehicula venenatis. Phasellus rutrum diam sed venenatis facilisis. Quisque consectetur accumsan
+                dignissim. Nullam aliquet lorem condimentum odio rhoncus maximus. Fusce aliquam quam eu pretium blandit.
+                Praesent ullamcorper, ipsum vel laoreet ultricies, dolor nunc ultricies felis, sed efficitur odio ex
+                tempor velit.</p>
+        </div>
+        <div class="column">
+            <img src="../Avengers/image/age%20of%20ultron.jpg">
+            <p>Lorem ipsum dolor sit amet, consectetur adipiscing elit. Donec varius malesuada ante, id vehicula felis.
+                Cras sit amet neque orci. Ut sit amet euismod leo. Orci varius natoque penatibus et magnis dis
+                parturient montes, nascetur ridiculus mus. Mauris mattis neque sed magna scelerisque cursus. Aliquam
+                tincidunt dignissim quam ac lacinia. Nulla quis posuere ex. Aliquam non lacinia enim. Vestibulum posuere
+                vehicula venenatis. Phasellus rutrum diam sed venenatis facilisis. Quisque consectetur accumsan
+                dignissim. Nullam aliquet lorem condimentum odio rhoncus maximus. Fusce aliquam quam eu pretium blandit.
+                Praesent ullamcorper, ipsum vel laoreet ultricies, dolor nunc ultricies felis, sed efficitur odio ex
+                tempor velit.</p>
+        </div>
+        <div class="column">
+            <img src="../Avengers/image/infinity%20war.png">
+            <p>Lorem ipsum dolor sit amet, consectetur adipiscing elit. Donec varius malesuada ante, id vehicula felis.
+                Cras sit amet neque orci. Ut sit amet euismod leo. Orci varius natoque penatibus et magnis dis
+                parturient montes, nascetur ridiculus mus. Mauris mattis neque sed magna scelerisque cursus. Aliquam
+                tincidunt dignissim quam ac lacinia. Nulla quis posuere ex. Aliquam non lacinia enim. Vestibulum posuere
+                vehicula venenatis. Phasellus rutrum diam sed venenatis facilisis. Quisque consectetur accumsan
+                dignissim. Nullam aliquet lorem condimentum odio rhoncus maximus. Fusce aliquam quam eu pretium blandit.
+                Praesent ullamcorper, ipsum vel laoreet ultricies, dolor nunc ultricies felis, sed efficitur odio ex
+                tempor velit.</p>
+        </div>
+        <div class="column">
+            <img src="image/endgame.jpg">
+            <p>Lorem ipsum dolor sit amet, consectetur adipiscing elit. Donec varius malesuada ante, id vehicula felis.
+                Cras sit amet neque orci. Ut sit amet euismod leo. Orci varius natoque penatibus et magnis dis
+                parturient montes, nascetur ridiculus mus. Mauris mattis neque sed magna scelerisque cursus. Aliquam
+                tincidunt dignissim quam ac lacinia. Nulla quis posuere ex. Aliquam non lacinia enim. Vestibulum posuere
+                vehicula venenatis. Phasellus rutrum diam sed venenatis facilisis. Quisque consectetur accumsan
+                dignissim. Nullam aliquet lorem condimentum odio rhoncus maximus. Fusce aliquam quam eu pretium blandit.
+                Praesent ullamcorper, ipsum vel laoreet ultricies, dolor nunc ultricies felis, sed efficitur odio ex
+                tempor velit.</p>
+        </div>
+    </div>
+</div>
 </body>
 </html>
-
 
 <?php
 ?>

a226bbf065cd11498cb26853119f1e412951a758
ishak <ijmilou@roc-dev.com>
2021-09-23 13:41:54 +0200

    done with layout homepage

---
+++ Avengers/avengers.php
@@ -0,0 +1,17 @@
+<!DOCTYPE html>
+<html lang="en">
+<head>
+    <meta charset="UTF-8">
+    <title>Avengers</title>
+    <link href="image/avengersIcon.png" type="image/icon" rel="icon">
+
+</head>
+<body>
+
+hello world
+</body>
+</html>
+
+
+<?php
+?>
--- index.php
+++ index.php
@@ -3,24 +3,117 @@
 <head>
     <link href="style.css" rel="stylesheet">
     <meta charset="UTF-8">
-    <title>Title</title>
+    <title>Marvel</title>
+    <link href="images/img.png" type="image/icon" rel="icon">
 </head>
 <body>
+<header>
     <img id="headerImg" src="images/img.png" alt="homeImage marvel">
-
-    <div class="movies">
-        <div class="container">
+    <nav class="inlogButton">
+        <a href="inloggen">inloggen</a>
+        <a href="registreren">registreren</a>
+    </nav>
+    <nav>
+        <a href="Avengers/avengers.php">Avengers</a>
+        <a href="captainAmerica">captain America</a>
+        <a href="thor">thor</a>
+        <a href="ironMan">iron Man</a>
+        <a href="blackPanther">black Panther</a>
+    </nav>
+</header>
+<div id="HomeMainBody">
+    <h1>Upcoming Movies & Series</h1>
+    <div class="container">
+        <div class="column">
             <img src="images/antmanHome.jpg">
-            <p class="text"></p>
+            <p>Lorem ipsum dolor sit amet, consectetur adipiscing elit. Donec varius malesuada ante, id vehicula felis.
+                Cras sit amet neque orci. Ut sit amet euismod leo. Orci varius natoque penatibus et magnis dis
+                parturient montes, nascetur ridiculus mus. Mauris mattis neque sed magna scelerisque cursus. Aliquam
+                tincidunt dignissim quam ac lacinia. Nulla quis posuere ex. Aliquam non lacinia enim. Vestibulum posuere
+                vehicula venenatis. Phasellus rutrum diam sed venenatis facilisis. Quisque consectetur accumsan
+                dignissim. Nullam aliquet lorem condimentum odio rhoncus maximus. Fusce aliquam quam eu pretium blandit.
+                Praesent ullamcorper, ipsum vel laoreet ultricies, dolor nunc ultricies felis, sed efficitur odio ex
+                tempor velit.</p>
+        </div>
+        <div class="column">
             <img src="images/blackPantherHome.jpg">
+            <p>Lorem ipsum dolor sit amet, consectetur adipiscing elit. Donec varius malesuada ante, id vehicula felis.
+                Cras sit amet neque orci. Ut sit amet euismod leo. Orci varius natoque penatibus et magnis dis
+                parturient montes, nascetur ridiculus mus. Mauris mattis neque sed magna scelerisque cursus. Aliquam
+                tincidunt dignissim quam ac lacinia. Nulla quis posuere ex. Aliquam non lacinia enim. Vestibulum posuere
+                vehicula venenatis. Phasellus rutrum diam sed venenatis facilisis. Quisque consectetur accumsan
+                dignissim. Nullam aliquet lorem condimentum odio rhoncus maximus. Fusce aliquam quam eu pretium blandit.
+                Praesent ullamcorper, ipsum vel laoreet ultricies, dolor nunc ultricies felis, sed efficitur odio ex
+                tempor velit.</p>
+        </div>
+        <div class="column">
             <img src="images/doctorStrangeHome.jpg">
+            <p>Lorem ipsum dolor sit amet, consectetur adipiscing elit. Donec varius malesuada ante, id vehicula felis.
+                Cras sit amet neque orci. Ut sit amet euismod leo. Orci varius natoque penatibus et magnis dis
+                parturient montes, nascetur ridiculus mus. Mauris mattis neque sed magna scelerisque cursus. Aliquam
+                tincidunt dignissim quam ac lacinia. Nulla quis posuere ex. Aliquam non lacinia enim. Vestibulum posuere
+                vehicula venenatis. Phasellus rutrum diam sed venenatis facilisis. Quisque consectetur accumsan
+                dignissim. Nullam aliquet lorem condimentum odio rhoncus maximus. Fusce aliquam quam eu pretium blandit.
+                Praesent ullamcorper, ipsum vel laoreet ultricies, dolor nunc ultricies felis, sed efficitur odio ex
+                tempor velit.</p>
+        </div>
+        <div class="column">
             <img src="images/eternalsHome.jpg">
+            <p>Lorem ipsum dolor sit amet, consectetur adipiscing elit. Donec varius malesuada ante, id vehicula felis.
+                Cras sit amet neque orci. Ut sit amet euismod leo. Orci varius natoque penatibus et magnis dis
+                parturient montes, nascetur ridiculus mus. Mauris mattis neque sed magna scelerisque cursus. Aliquam
+                tincidunt dignissim quam ac lacinia. Nulla quis posuere ex. Aliquam non lacinia enim. Vestibulum posuere
+                vehicula venenatis. Phasellus rutrum diam sed venenatis facilisis. Quisque consectetur accumsan
+                dignissim. Nullam aliquet lorem condimentum odio rhoncus maximus. Fusce aliquam quam eu pretium blandit.
+                Praesent ullamcorper, ipsum vel laoreet ultricies, dolor nunc ultricies felis, sed efficitur odio ex
+                tempor velit.</p>
+        </div>
+        <div class="column">
             <img src="images/hawkeyeHome.webp">
+            <p>Lorem ipsum dolor sit amet, consectetur adipiscing elit. Donec varius malesuada ante, id vehicula felis.
+                Cras sit amet neque orci. Ut sit amet euismod leo. Orci varius natoque penatibus et magnis dis
+                parturient montes, nascetur ridiculus mus. Mauris mattis neque sed magna scelerisque cursus. Aliquam
+                tincidunt dignissim quam ac lacinia. Nulla quis posuere ex. Aliquam non lacinia enim. Vestibulum posuere
+                vehicula venenatis. Phasellus rutrum diam sed venenatis facilisis. Quisque consectetur accumsan
+                dignissim. Nullam aliquet lorem condimentum odio rhoncus maximus. Fusce aliquam quam eu pretium blandit.
+                Praesent ullamcorper, ipsum vel laoreet ultricies, dolor nunc ultricies felis, sed efficitur odio ex
+                tempor velit.</p>
+        </div>
+        <div class="column">
             <img src="images/spidermanHome.jpg">
+            <p>Lorem ipsum dolor sit amet, consectetur adipiscing elit. Donec varius malesuada ante, id vehicula felis.
+                Cras sit amet neque orci. Ut sit amet euismod leo. Orci varius natoque penatibus et magnis dis
+                parturient montes, nascetur ridiculus mus. Mauris mattis neque sed magna scelerisque cursus. Aliquam
+                tincidunt dignissim quam ac lacinia. Nulla quis posuere ex. Aliquam non lacinia enim. Vestibulum posuere
+                vehicula venenatis. Phasellus rutrum diam sed venenatis facilisis. Quisque consectetur accumsan
+                dignissim. Nullam aliquet lorem condimentum odio rhoncus maximus. Fusce aliquam quam eu pretium blandit.
+                Praesent ullamcorper, ipsum vel laoreet ultricies, dolor nunc ultricies felis, sed efficitur odio ex
+                tempor velit.</p>
+        </div>
+        <div class="column">
             <img src="images/thorHome.jpg">
+            <p>Lorem ipsum dolor sit amet, consectetur adipiscing elit. Donec varius malesuada ante, id vehicula felis.
+                Cras sit amet neque orci. Ut sit amet euismod leo. Orci varius natoque penatibus et magnis dis
+                parturient montes, nascetur ridiculus mus. Mauris mattis neque sed magna scelerisque cursus. Aliquam
+                tincidunt dignissim quam ac lacinia. Nulla quis posuere ex. Aliquam non lacinia enim. Vestibulum posuere
+                vehicula venenatis. Phasellus rutrum diam sed venenatis facilisis. Quisque consectetur accumsan
+                dignissim. Nullam aliquet lorem condimentum odio rhoncus maximus. Fusce aliquam quam eu pretium blandit.
+                Praesent ullamcorper, ipsum vel laoreet ultricies, dolor nunc ultricies felis, sed efficitur odio ex
+                tempor velit.</p>
+        </div>
+        <div class="column">
             <img src="images/whatifHome.png">
+            <p>Lorem ipsum dolor sit amet, consectetur adipiscing elit. Donec varius malesuada ante, id vehicula felis.
+                Cras sit amet neque orci. Ut sit amet euismod leo. Orci varius natoque penatibus et magnis dis
+                parturient montes, nascetur ridiculus mus. Mauris mattis neque sed magna scelerisque cursus. Aliquam
+                tincidunt dignissim quam ac lacinia. Nulla quis posuere ex. Aliquam non lacinia enim. Vestibulum posuere
+                vehicula venenatis. Phasellus rutrum diam sed venenatis facilisis. Quisque consectetur accumsan
+                dignissim. Nullam aliquet lorem condimentum odio rhoncus maximus. Fusce aliquam quam eu pretium blandit.
+                Praesent ullamcorper, ipsum vel laoreet ultricies, dolor nunc ultricies felis, sed efficitur odio ex
+                tempor velit.</p>
         </div>
     </div>
+</div>
 </body>
 </html>
 

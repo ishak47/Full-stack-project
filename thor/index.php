969f780bab873275f4d91cf40a577fd960cebf03
ishak <ijmilou@roc-dev.com>
2021-09-27 12:56:31 +0200

    bezig met subpagina's.bijna allemaal af

---
+++ thor/index.php
@@ -0,0 +1,67 @@
+<!DOCTYPE html>
+<html lang="en">
+<head>
+    <link href="../Avengers/subpage.css" rel="stylesheet">
+    <meta charset="UTF-8">
+    <title>Marvel</title>
+    <link href="images/img.png" type="image/icon" rel="icon">
+</head>
+<body>
+<header>
+    <img id="headerImg" src="../images/img.png" alt="homeImage marvel">
+
+    <nav>
+        <a href="../index.php">home</a>
+        <a href="../Avengers/index.php">Avengers</a>
+        <a href="../captain%20america/index.php">Captain America</a>
+        <a href="ironMan">Iron Man</a>
+        <a href="blackPanther">Black Panther</a>
+    </nav>
+</header>
+<div id="HomeMainBody">
+    <div class="titels">
+        <h1>thor</h1>
+        <h1>thor <br> the dark world</h1>
+        <h1>thor <br> ragnarok</h1>
+    </div>
+    <div class="container">
+        <div class="column">
+            <img src="../thor/images/thor.jpg">
+            <p>Lorem ipsum dolor sit amet, consectetur adipiscing elit. Donec varius malesuada ante, id vehicula felis.
+                Cras sit amet neque orci. Ut sit amet euismod leo. Orci varius natoque penatibus et magnis dis
+                parturient montes, nascetur ridiculus mus. Mauris mattis neque sed magna scelerisque cursus. Aliquam
+                tincidunt dignissim quam ac lacinia. Nulla quis posuere ex. Aliquam non lacinia enim. Vestibulum posuere
+                vehicula venenatis. Phasellus rutrum diam sed venenatis facilisis. Quisque consectetur accumsan
+                dignissim. Nullam aliquet lorem condimentum odio rhoncus maximus. Fusce aliquam quam eu pretium blandit.
+                Praesent ullamcorper, ipsum vel laoreet ultricies, dolor nunc ultricies felis, sed efficitur odio ex
+                tempor velit.</p>
+        </div>
+        <div class="column">
+            <img src="../thor/images/the%20dark%20world.jpg">
+            <p>Lorem ipsum dolor sit amet, consectetur adipiscing elit. Donec varius malesuada ante, id vehicula felis.
+                Cras sit amet neque orci. Ut sit amet euismod leo. Orci varius natoque penatibus et magnis dis
+                parturient montes, nascetur ridiculus mus. Mauris mattis neque sed magna scelerisque cursus. Aliquam
+                tincidunt dignissim quam ac lacinia. Nulla quis posuere ex. Aliquam non lacinia enim. Vestibulum posuere
+                vehicula venenatis. Phasellus rutrum diam sed venenatis facilisis. Quisque consectetur accumsan
+                dignissim. Nullam aliquet lorem condimentum odio rhoncus maximus. Fusce aliquam quam eu pretium blandit.
+                Praesent ullamcorper, ipsum vel laoreet ultricies, dolor nunc ultricies felis, sed efficitur odio ex
+                tempor velit.</p>
+        </div>
+        <div class="column">
+            <img src="../thor/images/ragnarok.jpg">
+            <p>Lorem ipsum dolor sit amet, consectetur adipiscing elit. Donec varius malesuada ante, id vehicula felis.
+                Cras sit amet neque orci. Ut sit amet euismod leo. Orci varius natoque penatibus et magnis dis
+                parturient montes, nascetur ridiculus mus. Mauris mattis neque sed magna scelerisque cursus. Aliquam
+                tincidunt dignissim quam ac lacinia. Nulla quis posuere ex. Aliquam non lacinia enim. Vestibulum posuere
+                vehicula venenatis. Phasellus rutrum diam sed venenatis facilisis. Quisque consectetur accumsan
+                dignissim. Nullam aliquet lorem condimentum odio rhoncus maximus. Fusce aliquam quam eu pretium blandit.
+                Praesent ullamcorper, ipsum vel laoreet ultricies, dolor nunc ultricies felis, sed efficitur odio ex
+                tempor velit.</p>
+        </div>
+    </div>
+</div>
+</body>
+</html>
+
+<?php
+?>
